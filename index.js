763cac44fd11ab0e9d33026ce79467d1ad134832
violanerd <madalyne.cross@gmail.com>
2022-07-18 16:26:27 -0500

    functionality for the questions

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 // TODO: Include packages needed for this application
-const inquire = require('inquire');
+const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title? (Required)'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe your project'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions)
+    .then(data => {
+        return generateMarkdown(data);
+    })
+    .then(markdown => {
+        console.log(markdown);
+    })
+};
 
 // Function call to initialize app
 init();
